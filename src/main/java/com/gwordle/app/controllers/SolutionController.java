a3314f11fe5055453ae5a10481c668e188261f5d
claygilk <claygilkerson@gmail.com>
2022-12-28 15:21:44 -0500

    added delete and post endpoints

--- src/main/java/com/gwordle/app/controllers/SolutionController.java
+++ src/main/java/com/gwordle/app/controllers/SolutionController.java
@@ -1,9 +1,12 @@
 package com.gwordle.app.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +28,16 @@ public class SolutionController {
         return this.solutionRepository.getReferenceById(id);
     }
     
+    @GetMapping
+    @RequestMapping("/")
+    public List<Solution> getAllSolutions() {
+        
+        return this.solutionRepository.findAll();
+    
+    }
+
+    @PostMapping("/")
+    public void addSolution(@RequestBody Solution solution){
+        this.solutionRepository.saveAndFlush(solution);
+    }
 }
